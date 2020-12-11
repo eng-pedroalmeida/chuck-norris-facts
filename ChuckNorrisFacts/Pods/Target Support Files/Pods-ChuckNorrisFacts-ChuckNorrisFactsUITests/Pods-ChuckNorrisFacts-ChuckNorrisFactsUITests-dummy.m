8528ffc3399ed1c0354098d64329e057e9972df2
Pedro Almeida <eng.pedrobernardes@gmail.com>
2020-12-11 10:35:48 -0300

    Revert "Project Structure"

--- ChuckNorrisFacts/Pods/Target Support Files/Pods-ChuckNorrisFacts-ChuckNorrisFactsUITests/Pods-ChuckNorrisFacts-ChuckNorrisFactsUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ChuckNorrisFacts_ChuckNorrisFactsUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ChuckNorrisFacts_ChuckNorrisFactsUITests
-@end
